2930cafc99583b5a6a6e61f3f679fbd3ba8f9413
ZongSingHuang <58209813+ZongSingHuang@users.noreply.github.com>
2020-12-29 21:47:23 +0800

    2020.12.29

--- main_5050.py
+++ main_5050.py
@@ -11,38 +11,101 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import functools
+import time
 
+
+
+num_neig = 1
+g = 100
+p = 10
+times = 10
+w1 = 0.99
+w2 = 0.01
 np.random.seed(42)
 
+
+Xtr_all, ytr_all, Xts_all, yts_all = None, None, None, None
+# name_list = ['BreastCancer', 'BreastEW', 'Congress', 'Exactly', 'Exactly2', 
+#               'HeartEW', 'Ionosphere', 'KrVsKpEW', 'Lymphography', 'M-of-n',
+#               'PenglungEW', 'Sonar', 'SpectEW', 'Tic-tac-toe', 'Vote', 
+#               'WaveformEW', 'Wine', 'Zoo']
+name_list = ['Zoo']
+Xtr_all = []
+ytr_all = []
+Xts_all = []
+yts_all = []
+table = np.zeros((7, len(name_list)))
+table[3, :] = np.ones(len(name_list))*np.inf
+table[4, :] = -np.ones(len(name_list))*np.inf
+tmep_for_std = np.zeros((times, len(name_list)))
+tmep_for_loss = np.zeros((g, len(name_list)))
+
+
 # 讀資料
-Breastcancer = pd.read_csv('Breastcancer.csv', header=None).values
+for idx, name in enumerate(name_list):
+    data = pd.read_csv(name+'.csv', header=None).values
+    X_train, X_test, y_train, y_test = train_test_split(data[:, :-1], data[:, -1], test_size=0.5)
+    
+    Xtr_all.append(X_train)
+    ytr_all.append(y_train)
+    Xts_all.append(X_test)
+    yts_all.append(y_test)
 
-X_train, X_test, y_train, y_test = train_test_split(Breastcancer[:, :-1], Breastcancer[:, -1], stratify=Breastcancer[:, -1], test_size=0.5)
-
-def Breastcancer_test(x):
+def fitness(x, X_train, y_train, X_test, y_test):
     if x.ndim==1:
         x = x.reshape(1, -1)
     loss = np.zeros(x.shape[0])
     
     for i in range(x.shape[0]):
         if np.sum(x[i, :])>0:
-            knn = KNeighborsClassifier(n_neighbors=5).fit(X_train[:, x[i, :]], y_train)
-            score = accuracy_score(knn.predict(X_test[:, x[i, :]]), y_test)
-            loss[i] = 0.01*(1-score) + 0.99*(np.sum(x[i, :])/X_train.shape[1])
+            knn = KNeighborsClassifier(n_neighbors=1).fit(X_train[:, x[i, :].astype(bool)], y_train)
+            score = accuracy_score(knn.predict(X_test[:, x[i, :].astype(bool)]), y_test)
+            loss[i] = w1*(1-score) + w2*(np.sum(x[i, :])/X_train.shape[1])
         else:
             loss[i] = np.inf
     return loss
 
-optimizer = TMGWO(fit_func=Breastcancer_test, 
-                  num_dim=X_train.shape[1], num_particle=5, max_iter=30, x_max=1, x_min=0)
-optimizer.opt()
+for k in range(times):
+    for i in range(len(name_list)):
+        start = time.time()
+        
+        loss_func = functools.partial(fitness, X_train=Xtr_all[i], y_train=ytr_all[i], 
+                                      X_test=Xts_all[i], y_test=yts_all[i])
+        optimizer = TMGWO(fit_func=loss_func, 
+                          num_dim=Xtr_all[i].shape[1], num_particle=p, max_iter=g)
+        optimizer.opt()
+        
+        knn = KNeighborsClassifier(n_neighbors=num_neig)
+        knn.fit(Xtr_all[i][:, optimizer.X_alpha.astype(bool)], ytr_all[i])
+        
+        acc = accuracy_score(knn.predict(Xts_all[i][:, optimizer.X_alpha.astype(bool)]), yts_all[i])
+        selected = np.sum(optimizer.X_alpha)
+        loss = w1*(1-acc) + w2*selected/len(optimizer.X_alpha)
+        
+        table[0, i] += selected # for mean selected
+        table[1, i] += acc # for mean acc
+        table[2, i] += loss # for mean loss
+        table[6, i] += time.time()-start # for mean time
+        
+        tmep_for_std[k, i] = loss
+        tmep_for_loss[:, i] += optimizer.gBest_curve
+        print(i)
+    print('time:'+str(k))
 
-knn = KNeighborsClassifier(n_neighbors=5)
-knn.fit(X_train[:, optimizer.gBest_X], y_train)
-print(np.sum(optimizer.gBest_X))
-print(accuracy_score(knn.predict(X_test[:, optimizer.gBest_X]), y_test))
+table[0, :] = table[0, :] / times
+table[1, :] = table[1, :] / times
+table[2, :] = table[2, :] / times
+table[3, :] = tmep_for_std.min()
+table[4, :] = tmep_for_std.max()
+table[5, :] = np.std(tmep_for_std, axis=0)
+table[6, :] = table[6, :] / times
+table = pd.DataFrame(table)
+table = np.round(table, 2)
+table.columns=name_list
+table.index = ['mean feature selected', 'mean accuracy', 'mean fitness', 'best fitness', 'worst fitness', 'std fitness', 'time']
 
-knn = KNeighborsClassifier(n_neighbors=5)
-knn.fit(X_train, y_train)
-print(X_train.shape[1])
-print(accuracy_score(knn.predict(X_test), y_test))
+tmep_for_loss /= times
+tmep_for_loss = pd.DataFrame(tmep_for_loss)
+tmep_for_loss = np.round(tmep_for_loss, 2)
+tmep_for_loss.columns=name_list
--- main_9010.py
+++ main_9010.py
@@ -5,43 +5,96 @@ Created on Mon Nov 23 21:29:10 2020
 @author: ZongSing_NB
 """
 
-from TMGWO import TMGWO
+from BHPSOGWO import BHPSOGWO
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
+import functools
+import time
 
+num_neig = 5
+g = 100
+p = 10
+times = 20
+w1 = 0.99
+w2 = 0.01
+skf = StratifiedKFold(n_splits=10, shuffle=True)
 np.random.seed(42)
 
+# name_list = ['BreastCancer', 'BreastEW', 'Congress', 'Exactly', 'Exactly2', 
+#               'HeartEW', 'Ionosphere', 'KrVsKpEW', 'Lymphography', 'M-of-n',
+#               'PenglungEW', 'Sonar', 'SpectEW', 'Tic-tac-toe', 'Vote', 
+#               'WaveformEW', 'Wine', 'Zoo']
+name_list = ['BreastCancer', 'BreastEW', 'Congress', 
+              'HeartEW', 'Ionosphere', 'Lymphography',
+              'PenglungEW', 'Sonar', 'SpectEW', 'Vote', 'Wine', 'Zoo']
+X_all = []
+y_all = []
+table = np.zeros((7, len(name_list)))
+table[3, :] = np.ones(len(name_list))*np.inf
+table[4, :] = -np.ones(len(name_list))*np.inf
+tmep_for_std = np.zeros((times, len(name_list)))
+tmep_for_loss = np.zeros((g, len(name_list)))
+
 # 讀資料
-Breastcancer = pd.read_csv('Breastcancer.csv', header=None).values
+for idx, name in enumerate(name_list):
+    data = pd.read_csv(name+'.csv', header=None).values
+    X, y = data[:, :-1], data[:, -1]
+    
+    X_all.append(X)
+    y_all.append(y)
 
-X = Breastcancer[:, :-1]
-y = Breastcancer[:, -1]
-
-def Breastcancer_test(x):
+def fitness(x, X, y):
     if x.ndim==1:
         x = x.reshape(1, -1)
     loss = np.zeros(x.shape[0])
     
     for i in range(x.shape[0]):
         if np.sum(x[i, :])>0:
-            score = cross_val_score(KNeighborsClassifier(n_neighbors=5), X[:, x[i, :]], y, cv=skf)
-            loss[i] = 0.01*(1-score.mean()) + 0.99*(np.sum(x[i, :])/X.shape[1])
+            score = cross_val_score(KNeighborsClassifier(n_neighbors=num_neig), X[:, x[i, :].astype(bool)], y, cv=skf)
+            loss[i] = w1*(1-score.mean()) + w2*(np.sum(x[i, :])/X.shape[1])
         else:
             loss[i] = np.inf
     return loss
 
-skf = StratifiedKFold(n_splits=10, shuffle=True)
-optimizer = TMGWO(fit_func=Breastcancer_test, 
-                  num_dim=X.shape[1], num_particle=5, max_iter=30, x_max=1, x_min=0)
-optimizer.opt()
+for k in range(times):
+    for i in range(len(name_list)):
+        start = time.time()
+        
+        loss_func = functools.partial(fitness, X=X_all[i], y=y_all[i])
+        optimizer = BHPSOGWO(fit_func=loss_func, 
+                             num_dim=X_all[i].shape[1], num_particle=p, max_iter=g)
+        optimizer.opt()
+        
+        acc = cross_val_score(KNeighborsClassifier(n_neighbors=5), X_all[i][:, optimizer.X_alpha.astype(bool)], y_all[i], cv=skf).mean()
+        selected = np.sum(optimizer.X_alpha)
+        loss = w1*(1-acc) + w2*selected/len(optimizer.X_alpha)
+        
+        table[0, i] += selected # for mean selected
+        table[1, i] += acc # for mean acc
+        table[2, i] += loss # for mean loss
+        table[6, i] += time.time()-start # for mean time
+        
+        tmep_for_std[k, i] = loss
+        tmep_for_loss[:, i] += optimizer.gBest_curve
+        print(i)
+    print('time:'+str(k))
 
-score = cross_val_score(KNeighborsClassifier(n_neighbors=5), X[:, optimizer.gBest_X], y, cv=skf)
-print(np.sum(optimizer.gBest_X))
-print(score.mean())
+table[0, :] = table[0, :] / times
+table[1, :] = table[1, :] / times
+table[2, :] = table[2, :] / times
+table[3, :] = tmep_for_std.min()
+table[4, :] = tmep_for_std.max()
+table[5, :] = np.std(tmep_for_std, axis=0)
+table[6, :] = table[6, :] / times
+table = pd.DataFrame(table)
+table = np.round(table, 2)
+table.columns=name_list
+table.index = ['mean feature selected', 'mean accuracy', 'mean fitness', 'best fitness', 'worst fitness', 'std fitness', 'time']
 
-score = cross_val_score(KNeighborsClassifier(n_neighbors=5), X, y, cv=skf)
-print(X.shape[1])
-print(score.mean())
+tmep_for_loss /= times
+tmep_for_loss = pd.DataFrame(tmep_for_loss)
+tmep_for_loss = np.round(tmep_for_loss, 2)
+tmep_for_loss.columns=name_list
